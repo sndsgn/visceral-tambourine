d23a1710b10354a8cfbc6061300a75678fd4b399
Aaron Norby <aaron.norby@gmail.com>
2015-10-03 15:06:25 -0700

    Write protractor tests to create events, join events, and add songs
    Tests to see if songs are there after being added and that a player exists

--- tests/protractor-tests/e2eSpec.js
+++ tests/protractor-tests/e2eSpec.js
@@ -2,6 +2,7 @@
 
 describe('Yodel', function() {
   var createButton = element(by.css('.create-button'));
+  var joinButton = element(by.css('.join-button'));
 
   beforeEach(function() {
     browser.get('http://localhost:3030');
@@ -9,10 +10,49 @@ describe('Yodel', function() {
 
   it('should create an event', function() {
     createButton.click();
+    element(by.model('event')).sendKeys('yodelparty');
+    var realCreateButton = element(by.css('.create-button'));
+    realCreateButton.click();
 
-    expect(browser.getCurrentUrl()).toBe('http://localhost:3030/#/create');
+    expect(browser.getCurrentUrl()).toBe('http://localhost:3030/#/events/yodelparty');
   });
 
+  it('should join an event', function() {
+    element(by.model('event')).sendKeys('yodelparty');
+    joinButton.click();
+    expect(browser.getCurrentUrl()).toBe('http://localhost:3030/#/events/yodelparty');
+  });
+
+  it('should add songs to an event', function() {
+    // first create the event
+    createButton.click();
+    element(by.model('event')).sendKeys('yodelparty');
+    var realCreateButton = element(by.css('.create-button'));
+    realCreateButton.click();
+
+    // search for and add some songs
+    element(by.css('.add-button')).click();
+
+    element(by.model('searchTerm')).sendKeys('devo');
+    element(by.css('.search-button')).click();
+
+    var addButtons = element.all(by.css('.add-song'));
+    addButtons.first().click();
+
+    var showPlButton = element(by.css('.home-button'));
+    showPlButton.click();
+
+    // we expect the iframe player to exist
+    var iframes = element.all(by.css('iframe'));
+
+    expect(iframes.count()).toEqual(1);
+
+
+    // we expect one song in songs
+    var playlist = element.all(by.repeater('song in songs'));
+    expect(playlist.count()).toEqual(1);
+
+  });
 });
 
 /*
