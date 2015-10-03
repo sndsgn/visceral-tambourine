979685b7d33429750c73c21e7b30cef1bb9dad9a
Aaron Norby <aaron.norby@gmail.com>
2015-10-03 14:32:26 -0700

    Write first working protractor test

---
+++ tests/protractor-tests/e2eSpec.js
@@ -0,0 +1,48 @@
+/* globals browser, expect, element, by */
+
+describe('Yodel', function() {
+  var createButton = element(by.css('.create-button'));
+
+  beforeEach(function() {
+    browser.get('http://localhost:3030');
+  });
+
+  it('should create an event', function() {
+    createButton.click();
+
+    expect(browser.getCurrentUrl()).toBe('http://localhost:3030/#/create');
+  });
+
+});
+
+/*
+
+describe('Protractor Demo App', function() {
+  var firstNumber = element(by.model('first'));
+  var secondNumber = element(by.model('second'));
+  var goButton = element(by.id('gobutton'));
+  var latestResult = element(by.binding('latest'));
+  var history = element.all(by.repeater('result in memory'));
+
+  function add(a, b) {
+    firstNumber.sendKeys(a);
+    secondNumber.sendKeys(b);
+    goButton.click();
+  }
+
+  beforeEach(function() {
+    browser.get('http://juliemr.github.io/protractor-demo/');
+  });
+
+  it('should have a history', function() {
+    add(1, 2);
+    add(3, 4);
+
+    expect(history.count()).toEqual(2);
+
+    add(5, 6);
+
+    expect(history.count()).toEqual(0); // This is wrong!
+  });
+});
+*/
